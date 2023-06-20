05a7474634b75b5b0ab3f6dd4a36b85f0b7ff4d3
Jenny <90241693+Jenny3760@users.noreply.github.com>
2023-06-20 23:00:17 +0900

    [unrated] Title: 문자열 곱하기, Time: 0.03 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,5 +1,6 @@
 class Solution {
     public String solution(String my_string, int k) {
+        /*
         String answer = "";
         
         for(int i = 0; i < k; i++){
@@ -7,5 +8,7 @@ class Solution {
         }
         
         return answer;
+        */
+        return my_string.repeat(k);
     }
 }
