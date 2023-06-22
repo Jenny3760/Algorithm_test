27c3d8b32be3952e529cda71c9abe8ae80ba5458
Jenny <90241693+Jenny3760@users.noreply.github.com>
2023-06-22 22:08:11 +0900

    [unrated] Title: 코드 처리하기, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -6,14 +6,9 @@ class Solution {
         for(int i = 0; i < code.length(); i++){
             char c = code.charAt(i);
             
-            if( mode == 0 ){
-                if( c != '1' ) answer += i % 2 == 0 ? c : "";
-                else mode = 1;
-            } else {
-                if( c != '1' ) answer += i % 2 != 0 ? c : "";
-                else mode = 0;
-            }
-            
+            if( c == '1' ) mode = 1 - mode;
+            else if( mode == 0 && i % 2 == 0 ) answer += c;
+            else if( mode == 1 && i % 2 != 0 ) answer += c;            
         }
         
         return answer == "" ? "EMPTY" : answer;
