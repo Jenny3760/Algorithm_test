e447f12fd1069d86de273cc415fd4f8bd904f670
Jenny <90241693+Jenny3760@users.noreply.github.com>
2023-06-30 23:43:42 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 3.92 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,25 +1,21 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         int[] answer = new int[queries.length];
+        Arrays.fill(answer, -1);
 
-        int j = 0;
-
-        for(int[] query : queries){
+        for(int i = 0; i < queries.length; i++) {
+            int[] query = queries[i];
             int s = query[0];
             int e = query[1];
             int k = query[2];
 
-            int min = 1000000;
-
-            for(int i = s; i <= e; i++){
-                if( arr[i] > k ){
-                    if( min > arr[i] ) min = arr[i];
+            for (int j = s; j <= e; j++) {
+                if (arr[j] > k) {
+                    answer[i] = answer[i] == -1 ? arr[j] : Math.min(answer[i], arr[j]);
                 }
             }
-
-            if( min == 1000000 ) min = -1;
-            answer[j++] = min;
-
         }
         
         return answer;
