f16fc93a850b9d297c14595a7ff591b64b2ca7ec
Jenny <90241693+Jenny3760@users.noreply.github.com>
2023-07-03 21:41:18 +0900

    [unrated] Title: qr code, Time: 0.07 ms, Memory: 69.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181903. qr code/qr code.java
+++ 프로그래머스/unrated/181903. qr code/qr code.java
@@ -1,11 +1,15 @@
 class Solution {
     public String solution(int q, int r, String code) {
-        String answer = "";
+       // String answer = "";
+        
+        StringBuilder sb = new StringBuilder();
         
         for(int i = 0; i < code.length(); i++){
-            if( i % q == r ) answer += code.charAt(i);    
+           // if( i % q == r ) answer += code.charAt(i);    
+            if( i % q == r ) sb.append(code.charAt(i));
         }
         
-        return answer;
+       // return answer;
+        return sb.toString();
     }
 }
