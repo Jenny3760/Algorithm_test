5ee335814474ef723d2e940cc74364a3cafa1028
Jenny <90241693+Jenny3760@users.noreply.github.com>
2023-07-11 22:42:00 +0900

    [unrated] Title: 배열 만들기 4, Time: 5.65 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,11 +1,21 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
         int[] stk = {};
         
+        Stack<Integer> stack = new Stack<>();
+
+        int i = 0;
+        while( i < arr.length ){
+            if( stack.size() == 0 ) stack.add(arr[i++]);
+            else if( stack.peek() < arr[i] ) stack.add(arr[i++]);
+            else stack.pop();
+        }
+        
+        return stack.stream().mapToInt(j -> j).toArray();
+        
+        /*
         List<Integer> list = new ArrayList<>();
         
         int i = 0;
@@ -16,7 +26,7 @@ class Solution {
             else list.remove(list.size()-1);
         }
         
-        System.out.println(list.toString());
         return list.stream().mapToInt(j -> j).toArray();
+        */
     }
 }
