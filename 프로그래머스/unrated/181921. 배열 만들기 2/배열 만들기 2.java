d813885c803175ee5a70e1fa94c9ebcd14105bc1
Jenny <90241693+Jenny3760@users.noreply.github.com>
2023-07-11 22:21:06 +0900

    [unrated] Title: 배열 만들기 2, Time: 2.97 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.java
@@ -8,13 +8,22 @@ class Solution {
         
         List<Integer> list = new ArrayList<>();
 
+        /*
         for(int i = l; i <= r; i++){
             String numStr = Integer.toString(i);
             if( numStr.matches("^[05]*$")){
                 list.add(i);
             }
         }
+        */
+        
+        for (int i = 1; i < 64; i++) {
+            int num = Integer.parseInt(Integer.toBinaryString(i)) * 5;
 
+            if (l <= num && num <= r)
+                list.add(num);
+        }
+        
         if( list.size() == 0 ) list.add(-1);
         
         return list.stream().mapToInt(i -> i).toArray();
