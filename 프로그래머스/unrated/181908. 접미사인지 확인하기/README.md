35ed8dd78109a2db69ee5d6ed0f97b2b56a3281a
Jenny <90241693+Jenny3760@users.noreply.github.com>
2023-08-09 22:32:42 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/README.md
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 접미사인지 확인하기 - 181908 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181908) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181908#) 
 
 ### 성능 요약
 
-메모리: 79.1 MB, 시간: 0.09 ms
+메모리: 75.9 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -4,6 +4,7 @@ class Solution {
     public int solution(String my_string, String is_suffix) {
         int answer = 0;
         
+        /*
         List<String> list = new ArrayList<>();
         
         for(int i = 0; i < my_string.length(); i++){
@@ -11,6 +12,9 @@ class Solution {
         }
         
         answer = list.contains(is_suffix) ? 1 : 0;
+        */
+        
+        answer = my_string.endsWith(is_suffix) ? 1 : 0;
         
         return answer;
     }
