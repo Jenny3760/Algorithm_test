0f5fc426762ebec33fcf2a9b6529ad7f07385107
Jenny <90241693+Jenny3760@users.noreply.github.com>
2023-10-18 00:20:12 +0900

    [level unrated] Title: 배열 조각하기, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] arr, int[] query) {
-        
+        /*
         for(int i = 0; i < query.length; i++){
             if( i % 2 == 0 ){ // 짝수이면
                 arr = Arrays.copyOfRange(arr, 0, query[i]+1);
@@ -11,19 +11,20 @@ class Solution {
             }
         }
         return arr;
-        /*
+        */
+        
         int start = 0;
         int end = query.length-1;
         
         for(int i = 0; i < query.length; i++){
             if( i % 2 == 0 ){ // 짝수이면
-                end = query[i] + 1;
+                end = start + query[i] + 1;
             } else { // 홀수이면
                 start += query[i];
             }
         }
         
         return Arrays.copyOfRange(arr, start, end);
-        */
+        
     }
 }
