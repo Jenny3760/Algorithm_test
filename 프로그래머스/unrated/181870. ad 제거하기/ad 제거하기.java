a565670376c55e5502f7f21f6f6aad036917691b
Jenny <90241693+Jenny3760@users.noreply.github.com>
2023-11-06 22:14:37 +0900

    [level unrated] Title: ad 제거하기, Time: 0.87 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        List<String> list = new ArrayList<>();
+
+        for(String str : strArr){
+            if( !str.contains("ad") ) list.add(str);
+        }
+
+        String[] answer = list.toArray(new String[list.size()]);
+        
+        return answer;
+    }
+}
