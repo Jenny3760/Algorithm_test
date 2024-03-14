e40fb97ae21f026005d2e78bff1a6f840da9b6e6
Jenny <90241693+Jenny3760@users.noreply.github.com>
2024-03-14 22:39:32 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.39 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -1,13 +1,16 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[5];
 
+        Arrays.sort(num_list);
+        
+        for(int i = 0; i < 5; i++){
+            answer[i] = num_list[i];
+        }
+        
+        /*
         List<Integer> list = Arrays.stream(num_list).boxed().collect(Collectors.toList());
 
         Collections.sort(list);
@@ -15,6 +18,7 @@ class Solution {
         List<Integer> list2 = new ArrayList<>(list.subList(0,5));
 
         answer = list2.stream().mapToInt(i->i).toArray();
+        */
         
         return answer;
     }
